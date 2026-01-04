63ae0d01bcbea75c3617703cc26399ff44c6d226
sijanthapa171 <thapasijan329@gmail.com>
2026-01-04 20:44:17 +0545

    [FEAT]: Add presentation plugin (presenting.vim) with Ctrl + Shift + p keybinding

--- config/plugins/present.nix
+++ config/plugins/present.nix
@@ -3,11 +3,25 @@
   # Create markdown files with slide separators (# Slide Title) and present them
   # Usage:
   #   :PresentingStart - Start presentation mode
+  #   Ctrl+Shift+p - Start presentation mode (keybinding)
   #   n - Next slide
   #   p - Previous slide
   #   q - Quit presentation
   
   extraPlugins = with pkgs.vimPlugins; [
     presenting-vim
+  ];
+  
+  # Keybinding to start presentation with Ctrl+Shift+p
+  keymaps = [
+    {
+      mode = "n";
+      key = "<C-S-p>";
+      action = ":PresentingStart<CR>";
+      options = {
+        desc = "Start presentation mode";
+        silent = true;
+      };
+    }
   ];
 }
