ebbd2e478692ee86a201b481c835b872d02275f6
sijanthapa171 <thapasijan329@gmail.com>
2026-01-04 20:53:57 +0545

    [REF]: replace presenting.vim with ppt.nvim plugin for markdown presentations with code execution

--- config/plugins/present.nix
+++ config/plugins/present.nix
@@ -1,15 +1,42 @@
 {pkgs, ...}: {
-  # Presenting.vim - Slides presentation in Neovim
-  # Create markdown files with slide separators (# Slide Title) and present them
+  # ppt.nvim - Make PPT slides from markdown files in Neovim
+  # Features:
+  #   - Create presentations from markdown
+  #   - Execute code blocks during presentation
+  #   - Simple navigation
   # Usage:
-  #   :PresentingStart - Start presentation mode
+  #   :PptStart - Start presentation mode
   #   Ctrl+Shift+p - Start presentation mode (keybinding)
   #   n - Next slide
   #   p - Previous slide
-  #   q - Quit presentation
+  #   X - Execute code block
+  #   q or <Esc> - Quit presentation
   
-  extraPlugins = with pkgs.vimPlugins; [
-    presenting-vim
+  extraPlugins = [
+    {
+      plugin = pkgs.vimUtils.buildVimPlugin {
+        name = "ppt.nvim";
+        src = pkgs.fetchFromGitHub {
+          owner = "kulvirdotgg";
+          repo = "ppt.nvim";
+          rev = "trunk";
+          sha256 = "sha256-no0jTGfa/2kpUNWHC+/A+kBg1NF8fHtnSGLpU24ysnQ=";
+        };
+      };
+      config = ''
+        lua << EOF
+        require("ppt").setup({
+          executors = {
+            python = "python3",
+            javascript = "node",
+            lua = "lua",
+            bash = "bash",
+            nix = "nix-instantiate --eval",
+          },
+        })
+        EOF
+      '';
+    }
   ];
   
   # Keybinding to start presentation with Ctrl+Shift+p
@@ -17,9 +44,9 @@
     {
       mode = "n";
       key = "<C-S-p>";
-      action = ":PresentingStart<CR>";
+      action = ":PptStart<CR>";
       options = {
-        desc = "Start presentation mode";
+        desc = "Start PPT presentation mode";
         silent = true;
       };
     }
